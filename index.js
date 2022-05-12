14152fefd284f3896056bdfa176be828dd2a5945
Owen Rossi-Keen <owenrossikeen@gmail.com>
2022-05-12 16:02:31 -0400

    Updated documentation

--- index.js
+++ index.js
@@ -1,33 +1,74 @@
-// https://jsdoc.app/index.html
-
 /**
- * @namespace helperFunctions
+ * @author Owen Rossi-Keen
+ * https://owenrossikeen.com/
  */
 
-/**
- * @namespace Launch Functions
- */
+// []==============[]
+// [] DEPENDENCIES []
+// []==============[]
 
 // Dependencies
-var ByteBuffer = require('bytebuffer');
+const ByteBuffer = require('bytebuffer');
 const CryptoJS = require('crypto-js');
 const readline = require('readline');
 const fs = require('fs');
 const recursive = require('recursive-readdir');
 
-// Helper functions
+// []============[]
+// [] NAMESPACES []
+// []============[]
+
+/** @namespace Definitions */
+/** @namespace helperFunctions */
+/** @namespace launchFunctions */
+
+// []=============[]
+// [] DEFINITIONS []
+// []=============[]
+
+/**
+ * @memberof Definitions
+ * The size of the prefix required to denote the size of a TobComm encoded string.
+ */
+const STRING_LENGTH_PREFIX_SIZE = 2;
+
+/**
+ * @memberof Definitions
+ * A string containing TobComm valid characters. Currently unused.
+ */
+const SANCTIFIED_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+// []==================[]
+// [] HELPER FUNCTIONS []
+// []==================[]
+
+/**
+ * @param {string} str
+ * @returns ByteBuffer containing SHA256 encoded str
+ * @memberof helperFunctions
+ */
 const stringToSHA256 = (str) => {
-  return new ByteBuffer(32).append(
-    Buffer.from(CryptoJS.SHA256(str).toString(CryptoJS.enc.Hex), 'hex')
-  );
+  return new ByteBuffer(32)
+    .append(Buffer.from(CryptoJS.SHA256(str).toString(CryptoJS.enc.Hex), 'hex'))
+    .reset();
 };
 
+/**
+ * @param {string} str
+ * @returns Byte Array containing encoded str
+ * @memberof helperFunctions
+ */
 const stringToByteBuffer = (str) => {
   const bb = new ByteBuffer(str.length);
   bb.writeString(str);
   return bb.buffer;
 };
 
+/**
+ * @param {bytearray} bytearray
+ * @returns Decoded 64-bit Floating Point number
+ * @memberof helperFunctions
+ */
 const intFromBytes = (byteArray) => {
   if (byteArray.length !== 4) return console.log('Yikes!');
   const bb = new ByteBuffer(4).append(byteArray);
@@ -40,9 +81,20 @@ const intFromBytes = (byteArray) => {
   );
 };
 
+/**
+ * Useful for rounding single-precision floats (such as those encoded in a Java application)
+ * @param {number} float
+ * @returns Number rounded to the 100ths place.
+ * @memberof helperFunctions
+ */
 const roundFloat = (float) => Math.round(float * 100) / 100;
 
-const getTime = (getTime = 'full') => {
+/**
+ * @param {boolean} full - Whether or not function returns seconds. default: false.
+ * @returns Formatted string time
+ * @memberof helperFunctions
+ */
+const getTime = (full = false) => {
   // This is a hacky way to do it, but it works 🤷‍♀️
 
   let currentDate = new Date();
@@ -52,6 +104,11 @@ const getTime = (getTime = 'full') => {
   return currentDate.toJSON().slice(0, 19).split('T')[1];
 };
 
+/**
+ * Iterates over all files (excluding node_modules and the like) to determine the (roughly estimated) number of lines of code in a given project
+ * Asynchronous
+ * @memberof helperFunctions
+ */
 const projectAudit = async () => {
   const countLines = async (filteredPaths) =>
     new Promise((res, rej) => {
@@ -134,17 +191,30 @@ const projectAudit = async () => {
   return finalCount;
 };
 
-const crc32 = (r) => {
+/**
+ * Generates a crc32 checksum from a string
+ * @param {string} str - The stringified data to be converted.
+ * @returns crc32 checksum number
+ * @memberof helperFunctions
+ */
+const crc32 = (str) => {
   for (var a, o = [], c = 0; c < 256; c++) {
     a = c;
     for (var f = 0; f < 8; f++) a = 1 & a ? 3988292384 ^ (a >>> 1) : a >>> 1;
     o[c] = a;
   }
-  for (var n = -1, t = 0; t < r.length; t++)
-    n = (n >>> 8) ^ o[255 & (n ^ r.charCodeAt(t))];
+  for (var n = -1, t = 0; t < str.length; t++)
+    n = (n >>> 8) ^ o[255 & (n ^ str.charCodeAt(t))];
   return (-1 ^ n) >>> 0;
 };
-
+/**
+ * Takes a parsed JSON object which contains numbers read as strings and converts those strings to numbers.
+ * e.g. userID: "1234" would become userID: 1234.
+ * Also has the nasty tendency to turn empty arrays into zeroes.
+ * @param {object} object - The stringified data to be converted.
+ * @returns Object with numbers, rather than strings.
+ * @memberof helperFunctions
+ */
 const parsedJsonStringsToInt = (object) => {
   if (Array.isArray(object)) {
     return object.map((data) => parsedJsonStringsToInt(data));
@@ -175,35 +245,76 @@ const parsedJsonStringsToInt = (object) => {
   }
   return obj;
 };
-
+/**
+ * Converts degrees to radians. Simple as.
+ * @param {number} deg
+ * @memberof helperFunctions
+ */
 const degreesToRadians = (deg) => {
   return deg * (Math.PI / 180);
 };
 
+/**
+ * Converts radians to degrees. Simple as.
+ * @param {number} rad
+ * @memberof helperFunctions
+ */
 const radiansToDegrees = (rad) => {
   return rad * (180 / Math.PI);
 };
 
+/**
+ * Converts miles to kilometers. Simple as.
+ * @param {number} miles
+ * @memberof helperFunctions
+ */
 const milesToKilometers = (miles) => {
   return miles / 1.609;
 };
 
+/**
+ * Converts kilometers to miles. Simple as.
+ * @param {number} kilometers
+ * @memberof helperFunctions
+ */
 const kilometersToMiles = (kilometers) => {
   return kilometers * 1.609;
 };
 
-const bytesToHexString = (data) => {
-  let bb = new ByteBuffer(32).append(data).reset();
+/**
+ * Converts a byte array into a hexadecimal string.
+ * @param {bytearray} data - Data to be encoded
+ * @param {number} length - The maximum number of bytes which will be read (left justified). Default: 32.
+ * @memberof helperFunctions
+ */
+const bytesToHexString = (data, length = 32) => {
+  let bb = new ByteBuffer(length).append(data).reset();
   return bb.toHex();
 };
 
-// Selects a random value from an array
+/**
+ * Selects a pseudorandom value from the provided array.
+ * Because I was sick of typing 'Math.' 200 times.
+ * @param {array} arr - Array to be selected from.
+ * @memberof helperFunctions
+ */
 const arrayRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Capitalizes a string
+/**
+ * Capitalizes a given string.
+ * @param {string} str - String to be capitalized
+ * @memberof helperFunctions
+ */
 const capitalizeString = (str) => str.replace(/^\w/, (c) => c.toUpperCase());
 
-// Generates an ordinal suffix for a number
+/**
+ * Generates an ordinal suffix for a number.
+ * e.g. 1 becomes 1st, 9 becomes 9th.
+ * @param {number} i - The number for which an ordinal will be generated.
+ * @returns The number concatenated with its ordinal.
+ * @memberof helperFunctions
+ */
+
 const generateOrdinal = (i) => {
   var j = i % 10,
     k = i % 100;
@@ -219,11 +330,17 @@ const generateOrdinal = (i) => {
   return i + 'th';
 };
 
-// LAUNCH FUNCTIONS
+// []==================[]
+// [] LAUNCH FUNCTIONS []
+// []==================[]
 
-const STRING_LENGTH_PREFIX_SIZE = 2;
-const SANCTIFIED_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-
+/**
+ * Reads a Short at the provided ByteBuffer's offset, reads a string of the length denoted by the read short.
+ * This DOES iterate the ByteBuffer in the namespace where the function is called.
+ * @param {ByteBuffer} bb - The ByteBuffer from which the string will be read.
+ * @returns The string which was read.
+ * @memberof launchFunctions
+ */
 const stringFromData = (bb) => {
   let lengthToRead = bb.readShort();
 
@@ -232,6 +349,12 @@ const stringFromData = (bb) => {
   return cString;
 };
 
+/**
+ * Converts a string to a ByteBuffer containing a short (representing the length of the converted string), followed by the converted string.
+ * @param {string} str - The string which will be converted.
+ * @returns ByteArray
+ * @memberof launchFunctions
+ */
 const getStringData = (str) => {
   const bb = new ByteBuffer.allocate(getStringDataSize(str));
   cString = stringToBytesBB(str);
@@ -240,10 +363,23 @@ const getStringData = (str) => {
 
   return bb.buffer;
 };
+
+/**
+ * Returns the byte-size of a short (representing the length of the string), followed by a byte-encoded string
+ * @param {string} str - The string which will be measured.
+ * @returns Number
+ * @memberof launchFunctions
+ */
 const getStringDataSize = (str) => {
   return STRING_LENGTH_PREFIX_SIZE + stringToBytesBB(str).length;
 };
 
+/**
+ * Sanitized the given name for profanity, as well as dangerous characters. Not currently functional.
+ * @param {string} str - The string which will be sanitized.
+ * @returns The sanitized string.
+ * @memberof launchFunctions
+ */
 const sanitizeName = (str) => {
   // https://github.com/2Toad/Profanity
   let strName = str.trim();
@@ -264,8 +400,15 @@ const sanitizeName = (str) => {
   return strName;
 };
 
-// Cleans coarsely converted JSON (from XML) for use in Counterforce.
-const cleanJSON = (json) => {
+/**
+ * Cleans coarsely converted JSON game save (from XML) for use in Counterforce.
+ * This is an ultra-specific function for Counterforce.
+ * @param {object} json - The parsed JSON object.
+ * @returns Cleaned object.
+ * @memberof launchFunctions
+ */
+//
+const cleanJSONGameSave = (json) => {
   // This is assuming you manually removed the top-level Game: {} and Ctrl+F find-and-replaced all underscores with nothing.
   let parsedGame = this.parsedJsonStringsToInt(json);
 
@@ -393,6 +536,10 @@ const cleanJSON = (json) => {
   return output;
 };
 
+// []=========[]
+// [] EXPORTS []
+// []=========[]
+
 module.exports.helperFunctions = {
   stringToSHA256,
   stringToByteBuffer,
@@ -413,8 +560,9 @@ module.exports.helperFunctions = {
 };
 
 module.exports.launchFunctions = {
-  cleanJSON,
+  cleanJSONGameSave,
   getStringDataSize,
   getStringData,
   stringFromData,
+  sanitizeName,
 };
